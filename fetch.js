26579f220dd2aae151a3d121fb7b7110d982f770
IIMarietteII <136766093+IIMarietteII@users.noreply.github.com>
2025-06-03 22:36:53 +0900

    Add files via upload

--- fetch.js
+++ fetch.js
@@ -102,18 +102,6 @@ async function fetchComments() {
     }
 }
 
-    const csvContent = "data:text/csv;charset=utf-8," + rows.map(e => e.map(f => `"${f}"`).join(",")).join("\n");
-    const encodedUri = encodeURI(csvContent);
-    const link = document.createElement("a");
-    link.setAttribute("href", encodedUri);
-    link.setAttribute("download", "youtube_comments.csv");
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-}
-
-fetchComments();
-
 function downloadComments() {
     const rows = [["번호", "작성자", "내용", "좋아요수", "날짜", "타입"]];
     let index = 1;
@@ -136,3 +124,5 @@ function downloadComments() {
     link.click();
     document.body.removeChild(link);
 }
+
+fetchComments();
